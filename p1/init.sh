a2b9ad419297fee951f6f0d30d9b8f40dde7372e
DinoMalin <juliancario@orange.fr>
2025-06-01 23:05:59 +0200

    fix: using alpine because arch encounters bugs

--- p1/init.sh
+++ p1/init.sh
@@ -2,14 +2,15 @@
 
 SERVER_IP="192.168.56.110"
 TOKEN="/var/lib/rancher/k3s/server/node-token"
-CONFIG="/etc/rancher/k3s/k3s.yaml"
 
 if [ $1 = "server" ]; then
 	curl -sfL https://get.k3s.io | sh -
-	cat $TOKEN > /vagrant/token
 
-	# sudo systemctl daemon-reload
-	# sudo systemctl restart k3s
+	until [ -e $TOKEN ]; do
+		sleep 1
+	done
+
+	cat $TOKEN > /shared/token
 elif [ $1 = "agent" ]; then
-	curl -sfL https://get.k3s.io | K3S_URL=https://$SERVER_IP:6443 K3S_TOKEN=$(cat /vagrant/token) sh -
+	curl -sfL https://get.k3s.io | K3S_URL=https://$SERVER_IP:6443 K3S_TOKEN=$(cat /shared/token) sh -
 fi
