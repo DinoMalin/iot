499bfb9bad3e3e928310ea85b0e9959b4d00bb94
DinoMalin <juliancario@orange.fr>
2025-06-03 20:36:55 +0200

    feat: add the app1

--- p2/init.sh
+++ p2/init.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 curl -sfL https://get.k3s.io | sh -
+
+until kubectl get nodes 2>/dev/null; do
+	sleep 1
+done
+
+kubectl apply -f /shared/app1.yaml
+kubectl expose deployment nginx --type=NodePort --name=nginx-deployment
