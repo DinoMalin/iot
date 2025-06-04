74a1702e5aebfb2f2bbfa3da65e1323184244a06
DinoMalin <juliancario@orange.fr>
2025-06-04 21:16:37 +0200

    feat: add ingress and simple web apps

--- p2/init.sh
+++ p2/init.sh
@@ -2,9 +2,15 @@
 
 curl -sfL https://get.k3s.io | sh -
 
-until kubectl get nodes 2>/dev/null; do
+until kubectl get nodes 2>&1 2>/dev/null; do
+	sleep 1
+done
+
+until kubectl get namespaces 2>&1 >/dev/null; do
 	sleep 1
 done
 
 kubectl apply -f /shared/app1.yaml
-kubectl expose deployment nginx --type=NodePort --name=nginx-deployment
+kubectl apply -f /shared/app2.yaml
+kubectl apply -f /shared/app3.yaml
+kubectl apply -f /shared/ingress.yaml
